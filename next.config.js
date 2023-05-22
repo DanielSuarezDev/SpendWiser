25cf44670f097ea14ae4666d5bedaccf9a747354
DanielSuarezDev <danielsuarez3114@gmail.com>
2023-05-21 23:37:03 -0500

    pwa

--- next.config.js
+++ next.config.js
@@ -1,9 +1,15 @@
+const withPWA = require('next-pwa')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['lh3.googleusercontent.com'],
   },
+  pwa: {
+    dest: 'public',
+    disable: process.env.NODE_ENV === 'development',
+  },
 }
 
-module.exports = nextConfig
+module.exports = withPWA(nextConfig)
